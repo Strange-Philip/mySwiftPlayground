6cffb0fe4fe7f50b1081fe25c0c8f63e0083982f
Strange-Philip <philipabakahmensah@gmail.com.com>
2024-08-26 22:23:18 +0000

    Initial Commit

---
+++ TutorialPrototype/TutorialPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialPrototypeApp.swift
+//  TutorialPrototype
+//
+//  Created by Philip Abakah on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TutorialPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
